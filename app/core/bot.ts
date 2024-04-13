fc23d0f549baaef241e8b31e174afbd3e856db78
Olayiwola Ayoola <olaiwolaayoola@yahoo.com>
2024-04-13 23:14:01 +0100

    Initial commit

---
+++ app/core/bot.ts
@@ -0,0 +1,17 @@
+import * as command from "../functions/commands";
+
+/**
+ * Start bot
+ * =====================
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
+(async () => {
+	await command.quit();
+	await command.start();
+	await command.sendPhoto();
+	await command.launch();
+})();
